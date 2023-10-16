9def91689a51a35010026eaf5be76010371e28c3
Tramazaygues <146027421+Tramazaygues@users.noreply.github.com>
2023-10-16 19:58:44 -0300

    Add files via upload

---
+++ app/views/movie.view.php
@@ -0,0 +1,22 @@
+<?php
+
+class MovieView{
+
+    //Renderiza el home
+    public function renderHome($movies){
+        $titulo = "StreamingNet";
+        $subtitulo = "Busque su serie o pelicula";
+        include_once './templates/header.phtml';
+        include_once './templates/home.phtml';
+    }
+
+
+
+    //Muestra la lista de todas las peliculas
+    public function showMovieList($movies){
+        $titulo = "Peliculas";
+        $subtitulo = "Un listado de todas las peliculas";
+        include_once './templates/header.phtml';
+        include_once './templates/movieList.phtml';
+    }
+}
