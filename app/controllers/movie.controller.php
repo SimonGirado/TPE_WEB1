1ca2fec815ee569c5cbfed82dc310db26f48246c
Tramazaygues <146027421+Tramazaygues@users.noreply.github.com>
2023-10-16 19:57:09 -0300

    Add files via upload

---
+++ app/controllers/movie.controller.php
@@ -0,0 +1,185 @@
+<?php
+
+require_once './app/views/movie.view.php';
+require_once './app/models/movie.model.php';
+require_once './helpers/error.helper.php';
+require_once './helpers/auth.helper.php';
+
+class MovieController{
+
+    private $view;
+    private $model;
+
+    private $errorHelper;
+    private $authHelper;
+
+
+    public function __construct(){
+        $this->view = new MovieView();
+        $this->model = new MovieModel();
+        $this->errorHelper = new ErrorHelper();
+        $this->authHelper = new AuthHelper();
+    }
+
+
+
+    //Muestra el home
+    public function showHome(){
+        $movies = $this->model->getLinksMovies();
+        $this->view->renderHome($movies);
+    }
+
+
+
+    //Muestra el catalogo completo de peliculas y series
+    public function showAllMovies(){
+        $movies = $this->model->getAllMovies();
+        $this->view->showMovieList($movies);
+    }
+
+
+
+    //Muestra el detalle de una pelicula con una ID determinada
+    public function showMovieDetail($movie_id){
+        if ($this->validateData([$movie_id])){
+            $titulo = "Detalle";
+            $subtitulo = "Detalles de la pelicula";
+            include_once './templates/header.phtml';
+            $details = $this->model->getMovieDetail($movie_id);
+            if ($details){
+                require_once './templates/movieDetail.phtml';
+            }
+            else{
+                $this->errorHelper->showError("Pelicula no encontrada.");
+            }
+            
+        }
+    }
+
+
+
+    //Llama al formulario de peliculas
+    //Si se llama sin parametros, no precarga datos y se usa para agregar una nueva pelicula
+    //Para editar una pelicula, se le llama con editing = true y con el objeto de la pelicula a editar
+    public function movieForm($editing = null,$movie = null){
+        $this->authHelper->checkLoggedIn();
+        $platforms = $this->model->getAllPlatforms();
+        include_once './templates/header.phtml';
+        include_once './templates/formPelicula.phtml';
+        
+    }
+
+
+    //Recoge datos del Formulario y los inserta en la DB
+    public function addMovie(){
+        $this->authHelper->checkLoggedIn();
+        if ($this->validateData($_POST)){
+            $titulo = htmlspecialchars($_POST["titulo"], ENT_QUOTES, 'UTF-8');
+            $sinopsis = htmlspecialchars($_POST["sinopsis"], ENT_QUOTES, 'UTF-8');
+            $director = htmlspecialchars($_POST["director"], ENT_QUOTES, 'UTF-8');
+            $fecha = htmlspecialchars($_POST["fecha"], ENT_QUOTES, 'UTF-8');
+            $cast = htmlspecialchars($_POST["cast"], ENT_QUOTES, 'UTF-8');
+            $plataforma = htmlspecialchars($_POST["plataforma"], ENT_QUOTES, 'UTF-8');
+            $link_portada = htmlspecialchars($_POST["link_portada"], ENT_QUOTES, 'UTF-8');
+            $lastId = $this->model->POSTmovie($titulo,$sinopsis,$director,$fecha,$cast,$plataforma,$link_portada);
+
+            if ($lastId){
+                header("Location:".BASE_URL."peliculas");
+            }
+            else{
+                $this->errorHelper->showError("Ocurrio un error al realizarse la insercion");
+            }
+        }
+        else{
+            $this->errorHelper->showError("Deben rellenarse todos los campos del formulario");
+            
+           
+            $this->movieForm(false);  //Formulario sin edicion
+        }
+    }
+
+
+
+    //Elimina una pelicula de la BD a partir de una ID
+    public function removeMovie($movie_id){
+        $this->authHelper->checkLoggedIn();
+        if ($this->validateData([$movie_id])){
+            $affectedRows = $this->model->DELETEmovie($movie_id);
+
+            if ($affectedRows > 0 ){
+                header("Location:".BASE_URL."peliculas");
+            }
+            else{
+                $this->errorHelper->showError("Ocurrio un error al intentar eliminar");
+            }
+        }
+    }
+
+
+
+    //Edita una pelicula segun un ID determinado
+    public function editMovie($movie_id){
+        $this->authHelper->checkLoggedIn();
+        $movie = $this->model->getMovieById($movie_id); 
+        if ($movie){
+            $this->movieForm(true,$movie); //Se llama a la funcion indicandole que se va a editar
+            
+        }
+        else{
+            $this->errorHelper->showError("Pelicula no encontrada.");
+        }
+        
+    }
+
+
+
+    //Edita una pelicula segun una ID determinada (a traves del formulario anterior)
+    public function updateMovie($id){
+        $this->authHelper->checkLoggedIn();
+        if (!$_POST){
+            $this->errorHelper->showError("Falta informacion para actualizar datos.");
+            die();
+        }
+
+        //En la sanitizacion, las comillas son transformadas, pudiendose ser visibles en el string final
+        //El navegador no las toma en cuenta a la hora de procesar el HTML
+        if ($this->validateData($_POST)){
+            $titulo = htmlspecialchars($_POST["titulo"], ENT_QUOTES, 'UTF-8');
+            $sinopsis = htmlspecialchars($_POST["sinopsis"], ENT_QUOTES, 'UTF-8');
+            $director = htmlspecialchars($_POST["director"], ENT_QUOTES, 'UTF-8');
+            $fecha = htmlspecialchars($_POST["fecha"], ENT_QUOTES, 'UTF-8');
+            $cast = htmlspecialchars($_POST["cast"], ENT_QUOTES, 'UTF-8');
+            $plataforma = htmlspecialchars($_POST["plataforma"], ENT_QUOTES, 'UTF-8');
+            $link_portada = htmlspecialchars($_POST["link_portada"], ENT_QUOTES, 'UTF-8');
+            $affectedRows = $this->model->PUTmovie($id,$titulo,$sinopsis,$director,$fecha,$cast,$plataforma,$link_portada);
+
+            if ($affectedRows > 0){
+                header("Location:".BASE_URL."peliculas");
+            }
+            else{
+                $this->errorHelper->showError("Ocurrio un error al editar");
+            }
+
+        }
+        else{
+            $this->errorHelper->showError("Debe rellenar todos los campos");
+            $movie = $this->model->getMovieById($id);
+            $this->movieForm(true,$movie); //Formulario con edicion
+        }
+    }
+
+
+
+
+    //Comprueba que todos los elementos de un array esten seteados y no esten vacios
+    //Por lo general, sirve para comprobar los datos del arreglo $_POST
+    private function validateData($data){
+        foreach($data as $item){
+            if (empty($item) || !isset($item)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ app/controllers/platform.controller.php
@@ -0,0 +1,174 @@
+<?php
+
+require_once './app/views/platform.view.php';
+require_once './app/models/platform.model.php';
+require_once './helpers/error.helper.php';
+require_once './helpers/auth.helper.php';
+
+class platformController{
+    private $view;
+    private $model;
+
+    private $errorHelper;
+    private $authHelper;
+
+    public function __construct(){
+        $this->view = new PlatformView();
+        $this->model = new PlatformModel();
+        $this->errorHelper = new ErrorHelper();
+        $this->authHelper = new AuthHelper();
+    }
+
+    public function showHome(){
+        $this->view->renderHome();
+    }
+
+
+
+    //Muestra un listado de todas las plataformas de Streaming
+    public function showAllPlatforms(){
+        $platforms = $this->model->getAllPlatforms();
+        $this->view->showPlatformList($platforms);
+    }
+
+
+    //Muestra todas las peliculas de una plataforma dada
+    public function showAllMoviesPlatform($platform_id) {
+        if ($this->validateData([$platform_id])) {
+            $titulo = "Plataforma";
+            $subtitulo = "categoria";
+            include_once './templates/header.phtml';
+    
+            // info de la plataforma
+            $details = $this->model->getPlatformDetails($platform_id);
+    
+            if ($details) {
+                // todas las películas de la pagina que estan en la plataforma
+                $movies = $this->model->getAllMoviesByPlatform($platform_id);
+
+                require_once './templates/platformDetail.phtml';
+            } else {
+                $this->errorHelper->showError("Plataforma no encontrada.");
+            }
+        }
+    }
+
+    private function validateData($data){
+        foreach($data as $item){
+            if (empty($item) || !isset($item)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+
+
+
+    //Formulario de plataformas (Sin parametros = agregar, con parametros = editar)
+
+    public function platformForm($editing = null, $platform = null) {
+        $this->authHelper->checkLoggedIn();
+        include_once './templates/header.phtml';
+        require_once './templates/formPlataforma.phtml';
+
+    }
+
+
+    //Agrega una nueva plataforma a la BD
+    public function addPlatform() {
+        $this->authHelper->checkLoggedIn();
+        if(isset($_POST['disponibilidad_ar'])) {
+            $disponibilidad_ar = $_POST['disponibilidad_ar'];
+        }
+        else {
+            $disponibilidad_ar = '0';
+        }
+
+        if ($this->validateData($_POST)) {
+            $nombre = htmlspecialchars($_POST['nombre'], ENT_QUOTES, 'UTF-8');
+            $enlace = htmlspecialchars($_POST['enlace'], ENT_QUOTES, 'UTF-8');
+            $tipo_contenido = htmlspecialchars($_POST['tipo_contenido'], ENT_QUOTES, 'UTF-8');
+            $precio = htmlspecialchars($_POST['precio'], ENT_QUOTES, 'UTF-8');
+            $link_logo = htmlspecialchars($_POST['link_logo'], ENT_QUOTES, 'UTF-8');
+            $id_nueva = $this->model->POSTplatform($nombre, $enlace, $tipo_contenido, $disponibilidad_ar, $precio,$link_logo);
+            if ($id_nueva) {
+                header("Location:".BASE_URL."plataformas");
+            }
+            else {
+                $this->errorHelper->showError("no se pudo agregar la plataforma.");
+            }
+        }
+        else{
+            $this->errorHelper->showError("Deben rellenarse todos los campos del formulario");
+            $this->platformForm(false);
+        }
+    }
+
+
+    //Elimina una plataforma segun una ID dada
+    public function removePlatform($platform_id) {
+        $this->authHelper->checkLoggedIn();
+        try {
+            $affectedRows = $this->model->DELETEplatform($platform_id);
+            if($affectedRows>0) {
+                header("Location:".BASE_URL."plataformas");
+            }
+            else {
+                $this->errorHelper->showError("Plataforma no encontrada.");
+            }
+        }
+        catch (PDOException $e){
+            $this->errorHelper->showError("No es posible.");
+        }
+    }
+
+
+    //Comprueba que una plataforma exista y llama a su formulario de edicion
+    public function editPlatform($platform_id) {
+        $this->authHelper->checkLoggedIn();
+        $platform = $this->model->getPlatformById($platform_id);
+        if ($platform){
+            $this->platformForm(true,$platform);
+        }
+        else{
+            $this->errorHelper->showError("Plataforma no encontrada.");
+        }
+    }
+
+
+    //Actualiza una plataforma segun una ID dada
+    public function updatePlatform($platform_id) {
+        $this->authHelper->checkLoggedIn();
+        if(isset($_POST['disponibilidad_ar'])) {
+            $disponibilidad_ar = $_POST['disponibilidad_ar'];
+        }
+        else {
+            $disponibilidad_ar = '0';
+        }
+
+        if ($this->validateData($_POST)) {
+            $nombre = htmlspecialchars($_POST['nombre'], ENT_QUOTES, 'UTF-8');
+            $enlace = htmlspecialchars($_POST['enlace'], ENT_QUOTES, 'UTF-8');
+            $tipo_contenido = htmlspecialchars($_POST['tipo_contenido'], ENT_QUOTES, 'UTF-8');
+            $precio = htmlspecialchars($_POST['precio'], ENT_QUOTES, 'UTF-8');
+            $link_logo = htmlspecialchars($_POST['link_logo'], ENT_QUOTES, 'UTF-8');
+            $affectedRows = $this->model->PUTplatform($nombre, $enlace, $tipo_contenido, $disponibilidad_ar, $precio, $platform_id,$link_logo);
+            if ($affectedRows>0) {
+                header("Location:".BASE_URL."plataformas");
+            }
+            else {
+                $this->errorHelper->showError("no se pudo editar la plataforma.");
+            }
+        }
+        else{
+            $this->errorHelper->showError("Deben rellenarse todos los campos del formulario");
+            $platform = $this->model->getPlatformById($platform_id);
+            $this->platformForm(true,$platform);
+
+        }
+    }
+}
+?>
